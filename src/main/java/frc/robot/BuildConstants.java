c861352fbbd046cd82107d77c7029898346642f1
Stefan <BlyatBoio5@gmail.com>
2025-11-14 16:31:56 -0700

    Added BuildConstants and fixed deleted dependancies

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Mock_Kickoff_Repo_2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "1773a110753b04b6c83b23dcc9e721b22e3409e2";
+  public static final String GIT_DATE = "2025-09-26 19:38:29 EDT";
+  public static final String GIT_BRANCH = "building-block-crash-fix";
+  public static final String BUILD_DATE = "2025-09-26 19:46:12 EDT";
+  public static final long BUILD_UNIX_TIME = 1758930372710L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
